c73c0de11ef91bb48bef2799c824c2b486d5806d
DESKTOP-G7FR5GV\Pradeep Ramakrishnan <Pradeep.Ramakrishnan@essensys.tech>
2024-02-25 15:24:27 +0000

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,9 @@
+const amount = 9;
+
+if (amount < 10) {
+    console.log(`small no`);
+} else {
+    console.log(`large no`);
+}
+
+console.log(`first node app`);
---
+++ modules.js
@@ -0,0 +1,9 @@
+
+const names = require('./names');
+const sayHi = require('./utils');
+const data = require('./alternative-flavour');
+require('./mind-grenade');
+
+sayHi(`pradeep`);
+sayHi(names.john);
+sayHi(names.peter);
